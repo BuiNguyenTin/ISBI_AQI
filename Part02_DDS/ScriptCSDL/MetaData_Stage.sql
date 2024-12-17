060f6aa83483a558e6506d1ca0c58ae8a94672b7
Bui Nguyen Tin <buinguyentin28062003@gmail.com>
2024-12-17 14:43:17 +0700

    tin

---
+++ Part02_DDS/ScriptCSDL/MetaData_Stage.sql
@@ -0,0 +1,80 @@
+CREATE DATABASE METADB	--Tao MetaDatabase
+CREATE DATABASE STAGE	--Tao Stage
+GO
+USE METADB 
+
+GO
+--Tao bang DataFlow
+CREATE TABLE DATA_FLOW
+(
+	ID INT NOT NULL IDENTITY(1,1),
+	NAME VARCHAR(50) NOT NULL,
+	STATUS INT,
+	LSET DATETIME,
+	CET DATETIME,
+	CONSTRAINT PK_DATA_FLOW
+	PRIMARY KEY CLUSTERED (ID)
+)
+
+GO
+--Nap du lieu vao DataFlow
+INSERT INTO DATA_FLOW (NAME, STATUS, LSET, CET)
+VALUES ('AIR_QUALITY', 1, '2023-01-01 00:00:00','2024-01-01 00:00:00')
+INSERT INTO DATA_FLOW (NAME, STATUS, LSET, CET)
+VALUES('USCOUNTRIES', 1, '2023-01-01 00:00:00','2024-01-01 00:00:00')
+INSERT INTO DATA_FLOW (NAME, STATUS, LSET, CET)
+VALUES('DIM_STATES', 1, '2023-01-01 00:00:00','2024-01-01 00:00:00')
+INSERT INTO DATA_FLOW (NAME, STATUS, LSET, CET)
+VALUES('DIM_COUNTIES', 1, '2023-01-01 00:00:00','2024-01-01 00:00:00')
+INSERT INTO DATA_FLOW (NAME, STATUS, LSET, CET)
+VALUES('DIM_DATE', 1, '2023-01-01 00:00:00','2024-01-01 00:00:00')
+INSERT INTO DATA_FLOW (NAME, STATUS, LSET, CET)
+VALUES('FACT_AQI', 1, '2023-01-01 00:00:00','2024-01-01 00:00:00')
+INSERT INTO DATA_FLOW (NAME, STATUS, LSET, CET)
+VALUES('DIM_PARAMETER', 1, '2023-01-01 00:00:00','2024-01-01 00:00:00')
+
+delete from DATA_FLOW where Name = 'AIR_QUALITY'
+GO
+SELECT * FROM DATA_FLOW	--Xem DataFlow
+
+GO
+USE STAGE
+
+GO
+--Tao cac bang trong STAGE
+CREATE TABLE AIR_QUALITY_STAGE (
+    [State Name] varchar(50),
+    [county Name] varchar(255),
+    [State Code] INT,
+    [County Code] INT,
+    [Date] datetime,
+    [AQI] INT,
+    [Category] varchar(255),
+    [Defining Parameter] varchar(255),
+    [Defining Site] varchar(255),
+    [Number of Sites Reporting] INT,
+    [Created] datetime,
+    [Last Updated] datetime
+)
+GO
+
+CREATE TABLE USCOUNTRIES_STAGE (
+	county varchar(50),
+	county_ascii varchar(225),
+	county_full varchar(225),
+	county_flips int,
+	state_id varchar(50),
+	state_name varchar(225),
+	lat float,
+	lng float,
+	[population] int,
+	[Created] datetime,
+    [Last Updated] datetime
+)
+
+GO
+--Xem cac bang trong STAGE
+SELECT * FROM AIR_QUALITY_STAGE
+SELECT * FROM USCOUNTRIES_STAGE
+
+
---
+++ Part02_DDS/ScriptCSDL/NDS.sql
@@ -0,0 +1,70 @@
+GO
+CREATE DATABASE NDS_ISBI	--Tao NDS
+
+GO
+USE NDS_ISBI
+
+GO
+--DROP TABLE AQI_NDS
+--Tao cac bang trong NDS
+CREATE TABLE AQI_NDS (
+	[AirDataSK] INT IDENTITY (1, 1),
+	[Date] datetime,
+	[AQI] INT,
+	[Category] varchar(255),
+	[Defining Parameter] varchar(255),
+	[Defining Site] varchar(255),
+	[Number of Sites Reporting] INT,
+	[Created] datetime,
+    [Last Updated] datetime,
+	[sourceId] int,
+	FOREIGN KEY (CountySK) REFERENCES COUNTIES_NDS(CountySK),
+	[CountySK] INT NOT NULL,
+	--UNIQUE (CountySK, Date)
+
+	CONSTRAINT PK_AQI_NDS
+	PRIMARY KEY CLUSTERED (AirDataSK)
+)
+
+GO
+CREATE TABLE COUNTIES_NDS(
+	[CountySK] INT IDENTITY (1, 1),
+	[County Code] INT,
+	[county Name] varchar(255),
+	[county_ascii] varchar(225),
+	[county_full] varchar(225),
+	[county_flips] int,
+	[lat] float,
+	[lng] float,
+	[population] int,
+	[Created] datetime,
+    [Last Updated] datetime,
+	FOREIGN KEY (StateSK) REFERENCES STATES_NDS(StateSK),
+	[StateSK] INT NOT NULL,
+	[sourceId] int,
+
+	CONSTRAINT PK_COUNTY_NDS
+	PRIMARY KEY CLUSTERED (CountySK)
+)
+
+GO
+--DROP TABLE STATES_NDS
+CREATE TABLE [STATES_NDS](
+	[StateSK] INT IDENTITY (1, 1),
+	[state_id] varchar(50),
+	[State Code] INT,
+	[State Name] varchar(50),
+	[Created] datetime,
+    [Last Updated] datetime,
+	[sourceId] int,
+
+	CONSTRAINT PK_STARES_NDS
+	PRIMARY KEY CLUSTERED (StateSK)
+)
+
+GO
+--Xem cac bang trong NDS
+SELECT * FROM AQI_NDS
+SELECT * FROM COUNTIES_NDS
+SELECT * FROM STATES_NDS
+
